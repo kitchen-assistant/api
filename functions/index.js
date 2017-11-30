11af19ade882d0057c7ce8b94eef3df761b2cc1a
Jesse Peplinski <peplinski.jesse@gmail.com>
2017-11-30 07:52:48 -0500

    Move location, item, and expiration constants to top add function()
    Difficult to keep track of the locally declared variables, so moved them to the top

--- functions/index.js
+++ functions/index.js
@@ -200,6 +200,11 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
         console.log("setting add context");
         assistant.setContext(ADD_CONTEXT);
 
+         // extract the parameter values - parameter --> value
+        const item = assistant.getArgument(ITEM_CONTEXT);
+        const location = assistant.getArgument(LOCATION_CONTEXT); // locations --> fridge
+        const expiration = assistant.getArgument(DATE_CONTEXT);
+
         // SWITCH (The intent from the user)
         switch (intent) {
             case LOCATION_ADD:
@@ -208,27 +213,25 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
 
                 // console.log(JSON.stringify(assistant.getContext(LOCATION_CONTEXT).parameters, null, 4)); // for debugging
                 
-                const locationToAdd = assistant.getArgument(LOCATION_CONTEXT); // extract the parameter values; i.e. parameter --> value; locations --> fridge
-                
                 // Query for adding location (can probably break this up more)
                 // eventually can use the actual ID here we get when we authenticate
                 var addLocationQuery = db.collection('users')
                                          .doc('JESSE_HARDCODE_TEST_ID')
                                          .collection('locations')
-                                         .doc(locationToAdd.toString());
+                                         .doc(location.toString());
                             
                 var addLocationIfItDoesNotExist = addLocationQuery.get()
                     .then(doc => {
                         // IF(The document doesnt exist, add it to the locations in the database)
                         if (!doc.exists) {
-                            console.log(`No such document, adding '${locationToAdd}' to list of locations.`);
-                            assistant.ask(`[WEBHOOK] Okay, I will add ${locationToAdd} to your list of locations.`); // Tell user location has been added
+                            console.log(`No such document, adding '${location}' to list of locations.`);
+                            assistant.ask(`[WEBHOOK] Okay, I will add ${location} to your list of locations.`); // Tell user location has been added
                             addLocationQuery.set({}); // add to database (this works)
                         } 
                         // ELSE(The location exists, tell the user that it exists)
                         else {
-                            console.log(`Oops! '${locationToAdd}' exists in your list of locations. Data:`, doc.data());
-                            assistant.ask(`[WEBHOOK] Oops! ${locationToAdd} already exists in your list of locations.`); // tell user that the location already exists
+                            console.log(`Oops! '${location}' exists in your list of locations. Data:`, doc.data());
+                            assistant.ask(`[WEBHOOK] Oops! ${location} already exists in your list of locations.`); // tell user that the location already exists
                         }
                     })
                     // Catch any errors
@@ -241,38 +244,35 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
                 console.log("adding item and setting item context");
                 assistant.setContext(ITEM_CONTEXT);
 
-                const itemsToAdd = assistant.getArgument(ITEM_CONTEXT); // extract the parameter values; i.e. parameter --> value; locations --> fridge
-                const locationToAddWithItem = assistant.getArgument(LOCATION_CONTEXT); // extract the parameter values; i.e. parameter --> value; locations --> fridge
-
-                console.log(`Item to add ${itemsToAdd}; Location to add: ${locationToAddWithItem}`);
+                console.log(`Item to add ${item}; Location to add: ${location}`);
 
                 // IF(The user hasn't specified where to store items, ie; 'Add milk', ask them where to store the items )
-                if (locationToAddWithItem == null) {
-                    assistant.ask(`[WEBHOOK] Where would you like to store ${itemsToAdd}?`);
+                if (location == null) {
+                    assistant.ask(`[WEBHOOK] Where would you like to store ${item}?`);
                 }
                 // ELSE (Location is specified where to store items)
                 else {
                     var addItemQuery = db.collection('users')
                                          .doc('JESSE_HARDCODE_TEST_ID')
                                          .collection('locations')
-                                         .doc(locationToAddWithItem.toString())
+                                         .doc(location.toString())
                                          .collection('items')
-                                         .doc(itemsToAdd.toString());
+                                         .doc(item.toString());
 
-                    // TODO: Need to account for itemsToAdd as a list (array) and add that to the database
+                    // TODO: Need to account for item as a list (array) and add that to the database
                             
                     var addItemIfItDoesNotExistInLocation = addItemQuery.get()
                         .then(doc => {
                             // IF(The document doesnt exist, add it to the locations in the database)
                             if (!doc.exists) {
-                                console.log(`No such document, adding '${itemsToAdd}' to '${locationToAddWithItem}'`);
-                                assistant.ask(`[WEBHOOK] Okay, I will add ${itemsToAdd} to your ${locationToAddWithItem}.`);// Tell user location has been added
+                                console.log(`No such document, adding '${item}' to '${location}'`);
+                                assistant.ask(`[WEBHOOK] Okay, I will add ${item} to your ${location}.`);// Tell user location has been added
                                 addItemQuery.set({});
                             } 
                             // ELSE (The item exists in that location, tell the user that it exists)
                             else {
-                                console.log(`Oops! '${itemsToAdd}' already exists in your ${locationToAddWithItem}`);
-                                assistant.ask(`[WEBHOOK] Oops! '${itemsToAdd}' already exists in your ${locationToAddWithItem}`); // tell user that the location already exists
+                                console.log(`Oops! '${item}' already exists in your ${location}`);
+                                assistant.ask(`[WEBHOOK] Oops! '${item}' already exists in your ${location}`); // tell user that the location already exists
                             }
                         })
                         // Catch any errors
@@ -288,37 +288,33 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
                 assistant.setContext(ITEM_CONTEXT); // not sure if we need to set these.. still don't really know what setting context does
                 assistant.setContext(DATE_CONTEXT); // not sure if we need this either
 
-                const expirationDateToAdd = assistant.getArgument(DATE_CONTEXT); // extract the parameter values; i.e. parameter --> value; locations --> fridge
-                const locationToAddWithItemExpirationDate = assistant.getArgument(LOCATION_CONTEXT);
-                const itemsToAddToExpiration = assistant.getArgument(ITEM_CONTEXT); // extract the parameter values; i.e. parameter --> value; locations --> fridge
+                console.log(`Expiration to add: ${expiration}; Items to add: ${item}`);
 
-                console.log(`Expiration to add: ${expirationDateToAdd}; Items to add: ${itemsToAddToExpiration}`);
-
-                if(expirationDateToAdd == null && itemsToAddToExpiration == null) {
+                if(expiration == null && item == null) {
                     assistant.ask(`[WEBHOOK] What item would you like to set to expire?`);
                 }
-                else if(expirationDateToAdd == null) {
-                    assistant.ask(`[WEBHOOK] What expiration date would you like to set on ${itemsToAddToExpiration}? Try saying it like MM-DD-YYYY`);
+                else if(expiration == null) {
+                    assistant.ask(`[WEBHOOK] What expiration date would you like to set on ${item}? Try saying it like MM-DD-YYYY`);
                 }
-                else if(itemsToAddToExpiration == null) {
-                    assistant.ask(`[WEBHOOK] What item would you like to set the expiration date of ${expirationDateToAdd}`);
+                else if(item == null) {
+                    assistant.ask(`[WEBHOOK] What item would you like to set the expiration date of ${expiration}`);
                 }
-                else if(locationToAddWithItemExpirationDate == null) {
+                else if(location == null) {
                     assistant.ask(`[WEBHOOK] Where would you like to set these items to expire?`);
                 }
                 else {
                     var addExpirationQuery = db.collection('users')
                                             .doc('JESSE_HARDCODE_TEST_ID')
                                             .collection('locations')
-                                            .doc(locationToAddWithItemExpirationDate.toString())
+                                            .doc(location.toString())
                                             .collection('items')
-                                            .doc(itemsToAddToExpiration.toString());
+                                            .doc(item.toString());
                             
                     var addExpirationDateToItem = addExpirationQuery.get()
                         .then(doc => {
-                                assistant.ask(`[WEBHOOK] Okay, I will add an expiration date of ${expirationDateToAdd} to ${itemsToAddToExpiration} in your ${locationToAddWithItemExpirationDate}`);
+                                assistant.ask(`[WEBHOOK] Okay, I will add an expiration date of ${expiration} to ${item} in your ${location}`);
                                 addExpirationQuery.set({
-                                    'expiration-date': new Date(expirationDateToAdd)
+                                    'expiration-date': new Date(expiration)
                                 });
                             
                         })
@@ -335,16 +331,14 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
                 assistant.setContext(CART_CONTEXT);
                 assistant.setContext(ITEM_CONTEXT);
 
-                const itemToAddToCart = assistant.getArgument(ITEM_CONTEXT);
-
-                assistant.ask(`[WEBHOOK] Okay, I will add ${itemToAddToCart} to your cart.`);
+                assistant.ask(`[WEBHOOK] Okay, I will add ${item} to your cart.`);
 
                 // Probably just want to set a flag, inCart set to true or false for this.
                 var userDoc = db.collection('users').doc("JESSE_HARDCODE_TEST_ID");
 
                 var data = {
                     cart : {
-                        [itemToAddToCart]: {}
+                        [item]: {}
                     }
                 }
 
@@ -353,7 +347,6 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
                 break;
 
             default:
-                console.log("adding default");
                 assistant.ask('Hmm, I was not able to add that expiration date. Can you try something like, set item to expire in 2 weeks?');
                 break;
         }
